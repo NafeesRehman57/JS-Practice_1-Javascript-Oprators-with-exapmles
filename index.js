674f319ce9ac0aeaf5b4860b98e190e16c064bfc
Nafees Rehman <98480259+NafeesRehman57@users.noreply.github.com>
2022-09-05 17:16:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+let a = 45;
+let b = 36;
+
+// Arthmatic Oprators
+
+console.log("a+b =", a + b);
+console.log("a-b =", a - b);
+console.log("a/b =", a / b);
+console.log("a*b =", a * b);
+console.log("a**b =", a ** b);
+console.log("a%b =", a % b);
+console.log(a)
+
+console.log("a++ =", a++);
+console.log("a =", a);
+console.log("++a =", ++a);
+console.log("a =", a);
+console.log("a-- =", a--);
+console.log("a =", a);
+console.log("--a =", --a);
+console.log("a =", a);
+
+// Assign Oprators
+let c = 6
+c += 2
+console.log("c+=2 =", c)
+c -= 2
+console.log("c-=2 =", c)
+c *= 2
+console.log("c*=2 =", c)
+c **= 2
+console.log("c**=2 =", c)
+c %= 2
+console.log("c%=2 =", c)
+c /= 2
+console.log("c/=2 =", c)
+
+// Comparison Oprators
+
+let num1 = 3;
+let num2 = "3";
+
+console.log("num1 == num2", num1 == num2);
+console.log("num1 != num2", num1 != num2);
+console.log("num1 === num2", num1 === num2);
+console.log("num1 !== num2", num1 !== num2);
+console.log("num1 < num2", num1 < num2);
+console.log("num1 > num2", num1 > num2);
+console.log("num1 <= num2", num1 <= num2);
+console.log("num1 >= num2", num1 >= num2);
+
+
+// Logical Oprators
+
+let x = 1;
+let y = 2;
+
+console.log("&&", x < y && y >= x)
+console.log("&&", x < y && y < x)
+console.log("||", x < y && y >= x)
+console.log("||", x < y && y < x)
+console.log(!true)
